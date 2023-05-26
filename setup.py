2bf88f9b10b3decc02b7ad8ae523c51d024ff88d
Juan Perlas <44761462+Juanperlas@users.noreply.github.com>
2023-05-26 18:26:52 -0500

    Update setup.py
    Instalaciones de las librerias por setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,23 @@
-from distutils.core import setup
-import py2exe
+from setuptools import setup
 
-setup(console=['app.py'])
+setup(
+    name="paralelismo_examen",
+    version="0.1.0",
+    description="Preguntas respondidas del 1 al 3",
+    author="Juan Perlas",
+    author_email="juanarroyoperlas@gmail.com",
+    url="https://github.com/Juanperlas/paralelismo_examen",
+    install_requires=[
+        "flask",
+        "platform",
+        "psutil",
+        "re",
+        "subprocess",
+        "cpuinfo",
+        "pynvml",
+        "pandas",
+        "pythoncom",
+        "wmi",
+        "winreg",
+    ],
+)
